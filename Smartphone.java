f26a04ce77671ff9806bed95d2cd7abe45c261cf
sridevisureshbabu <105561541+sridevisureshbabu@users.noreply.github.com>
2022-05-28 13:02:46 +0100

    Add files via upload

--- Smartphone.java
+++ Smartphone.java
@@ -1,19 +1,27 @@
 package Week3.Day1;
 
- class Smartphone extends Androidphone{
+ public class Smartphone extends Androidphone{
 	
 	 public void connwhtsapp()
 	 {
 		 System.out.println("connectwhatsapp");
 	 }
+	 
+	 public void takevideo()
+	 {
+	 	System.out.println("Take video from child class");
+	 }
 
 	 public static void main(String args[])
 	 {
 		 Smartphone s= new Smartphone();
+		 Androidphone a=new Androidphone();
 		 s.makeCall();
 		 s.sendmsg();
 		 s.savecontact();
 		 s.takevideo();
+		 a.takevideo();
+		 
 		 s.connwhtsapp();
 		 
 	 }
